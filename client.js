f1f75f16f010b6e276467f9c15b284dfaf7ef5e8
Hamlet Shu <ahngo13@gmail.com>
2020-06-05 12:57:12 +0900

    add check logic

--- client.js
+++ client.js
@@ -3,16 +3,18 @@ function getPosts() {
     .get("https://jsonplaceholder.typicode.com/posts")
     .then((returnData) => {
       const data = returnData.data;
-      if (data) {
+      if (data.length > 0) {
         const posts = document.getElementById("posts");
         data.forEach((post) => {
-          console.log(post);
+          //   console.log(post);
           posts.innerHTML +=
             "<li><b>" + post.title + "</b> - " + post.body + "</li>";
         });
+      } else {
+        alert("데이터가 존재하지 않습니다.");
       }
     })
     .catch((err) => {
-      console.log(err);
+      alert("데이터 요청이 실패하였습니다. 다시 시도해주세요.");
     });
 }
